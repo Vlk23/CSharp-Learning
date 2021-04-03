64917565a778888f0ef9bccd26d81371caea4011
Volkan Günel <73136163+Vlk23@users.noreply.github.com>
2021-04-04 01:50:39 +0300

    Update Program.cs

--- GameProject/Program.cs
+++ GameProject/Program.cs
@@ -10,12 +10,12 @@ namespace GameProject
             gamerManager.Add(new Gamer
             {
                 Id = 1,
-                BirthYear = 1984,
-                FirstName = "ENGİN",
-                LastName = "DEMİROĞ",
+                BirthYear = 1987,
+                FirstName = "Volkan",
+                LastName = "Gunel",
                 IdentityNumber = 12345
             });
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Merhaba!");
         }
     }
 }
